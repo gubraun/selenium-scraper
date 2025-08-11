f584e6df23cd766ed502cefbbbd88d467af250aa
Gunnar Braun <gunnar.braun@gmx.de>
2025-08-11 09:44:04 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,31 +1,39 @@
-from selenium.webdriver import Chrome
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.common.by import By
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.common.exceptions import NoSuchElementException, TimeoutException
-from webdriver_manager.chrome import ChromeDriverManager
-from webdriver_manager.core.os_manager import ChromeType
+from selenium.webdriver.chrome.service import Service
+import chromedriver_autoinstaller
+from pyvirtualdisplay import Display
+display = Display(visible=0, size=(800, 800))  
+display.start()
 
-options = Options()
-options.add_argument('--headless')
-options.add_argument('--no-sandbox')
-options.add_argument('--disable-dev-shm-usage')
-options.add_argument('--disable-gpu')
-options.add_argument('--disable-extensions')
-options.add_argument('--start-maximized')
-# Memory optimization
-options.add_argument('--disk-cache-size=1')
-options.add_argument('--media-cache-size=1')
-options.add_argument('--incognito')
-options.add_argument('--remote-debugging-port=9222')
-options.add_argument('--aggressive-cache-discard')
+chromedriver_autoinstaller.install()  # Check if the current version of chromedriver exists
+                                      # and if it doesn't exist, download it automatically,
+                                      # then add chromedriver to path
 
-service = Service('/usr/local/bin/chromedriver')
+chrome_options = webdriver.ChromeOptions()
+# Add your options as needed
+options = [
+   # Define window size here
+    "--window-size=1200,1200",
+    "--ignore-certificate-errors"
+ 
+    #"--headless",
+    #"--window-size=1920,1200",
+    #"--ignore-certificate-errors",
+    #"--disable-extensions",
+    # These flags BELOW are recommended for stability when running Chrome in headless or containerized environments (such as GitHub Actions).
+    "--disable-gpu",
+    "--no-sandbox",
+    "--disable-dev-shm-usage",
+    '--remote-debugging-port=9222'
+]
+for option in options:
+    chrome_options.add_argument(option)
+
     
-driver = webdriver.Chrome(service=service, options=options)
-driver.get('google.com')
+driver = webdriver.Chrome(options = chrome_options)
 
-driver.quit()
+driver.get('http://github.com')
+print(driver.title)
+with open('./GitHub_Action_Results.txt', 'w') as f:
+    f.write(f"This was written with a GitHub action {driver.title}")
